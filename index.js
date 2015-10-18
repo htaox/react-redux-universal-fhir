50bfa687e32c828149debbe316baed5b4d8414c1
htaox <htaox@hotmail.com>
2015-10-18 06:30:22 -0400

    Format with spaces

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ var valuesetsDest = 'schemas/valuesets';
 function setup() {
 
   var folders = ['schemas', tmpDest, baseDest, typesDest, resourcesDest, valuesetsDest];
-  for(var i in folders) {
-    if (!fs.existsSync(folders[i])){
+  for (var i in folders) {
+    if (!fs.existsSync(folders[i])) {
       fs.mkdirSync(folders[i]);
     }
   }
@@ -23,57 +23,57 @@ function setup() {
 
 function recurse(json) {
 
-  for (var x in json){
-  
-    if (Object.prototype.toString.call(json[x]) == '[object Array]'){
-      for(var y in json[x]){
-        
-        if (typeof json[x][y] === 'object'){
+  for (var x in json) {
+
+    if (Object.prototype.toString.call(json[x]) == '[object Array]') {
+      for (var y in json[x]) {
+
+        if (typeof json[x][y] === 'object') {
           recurse(json[x][y]);
         }
       }
     }
 
-    if (typeof json[x] === 'object'){
+    if (typeof json[x] === 'object') {
       recurse(json[x]);
     }
-    
-    if (x == 'type'){
+
+    if (x == 'type') {
       var type = types[json[x]];
-      if (type){
+      if (type) {
         //json[x] = type;
-        for(var k in types[type]) {
+        for (var k in types[type]) {
           json[k] = types[type][k];
         }
-      } 
+      }
     }
 
   }
 
-  if (typeof obj == 'object'){
+  if (typeof obj == 'object') {
     recurse(obj);
   }
 }
 
-function processBase(dir){
+function processBase(dir) {
   var files = fs.readdirSync(dir);
 
-  for(var i in files) {
+  for (var i in files) {
     if (files[i].search(/^type/i) != -1 && files[i].search(/(period)/i) != -1) {
       var done = processSchema(dir + '/' + files[i]);
       types[done.model] = done.schema;
     }
-  }  
+  }
 }
 
 function processTypes(dir) {
 
   var files = fs.readdirSync(dir);
 
-  for(var i in files) {
+  for (var i in files) {
     if (files[i].search(/^type/i) != -1 && files[i].search(/restful/i) == -1) {
       var done = processSchema(dir + '/' + files[i]);
-      types[done.model] = done.schema; 
+      types[done.model] = done.schema;
     }
   }
 
@@ -87,30 +87,35 @@ function processTypes(dir) {
 
 function updateObjectPath(obj, path, value) {
 
-  if (path.search(/extension|contained|text/i)==-1) {
+  if (path.search(/extension|contained|text/i) == -1) {
     objectPath.set(obj, path, value);
   }
 }
 
 function processValueset(file) {
-  var raw = fs.readFileSync(file, {encoding:'utf8'});
+  var raw = fs.readFileSync(file, {
+    encoding: 'utf8'
+  });
   var json = JSON.parse(raw);
-  if (!json.define){
-    console.log('Skipping => '+file);
+  if (!json.define) {
+    console.log('Skipping => ' + file);
     return;
   }
 
-  console.log('Processing => '+file);
+  console.log('Processing => ' + file);
   var model = json['name'];
   var obj = {};
-  obj.title = { type: "String", default: json.description };
+  obj.title = {
+    type: "String",
+    default: json.description
+  };
   obj.concept = json['define']['concept'];
-  for(var i in obj.concept) {
-    obj.concept[i].type = 'String';    
+  for (var i in obj.concept) {
+    obj.concept[i].type = 'String';
   }
   //
   fs.writeFileSync(valuesetsDest + '/' + path.basename(file, '.json') + '.json', JSON.stringify(obj, null, '  '));
-  
+
 }
 
 function processSchema(file) {
@@ -118,51 +123,61 @@ function processSchema(file) {
   /*
     Place loadTypes() here and not processAllSchemas so we can do unit test
   */
-  if (Object.keys(types) == 0){
+  if (Object.keys(types) == 0) {
     loadTypes();
   }
 
-  var raw = fs.readFileSync(file, {encoding:'utf8'});
+  var raw = fs.readFileSync(file, {
+    encoding: 'utf8'
+  });
   var json = JSON.parse(raw);
-  if (!json.structure){
-    console.log('Skipping => '+file);
+  if (!json.structure) {
+    console.log('Skipping => ' + file);
     return;
   }
-  console.log('Processing => '+file);
+  console.log('Processing => ' + file);
   var structure = json.structure[0];
   var element = structure.element;
   var searchParam = structure.searchParam;
 
   var model = element[0].path;
-  
+
   var obj = {};
 
-  for(var i in element) {
+  for (var i in element) {
     //console.log(element[i].definition.type);
     //min == max 1 == 1?
-    
+
     var el = element[i];
-    
+
     //We need to handle property collection (note:lowercase "c" b/c mongoose keyword for creating schema)
     //Capitalizing it fixes problem
-    el.path = el.path.replace(/collection/g,'Collection');
+    el.path = el.path.replace(/collection/g, 'Collection');
 
     if (i > 0 && el.definition && el.definition.type && el.definition.type.length > 0) {
-      
-      var val = { 
-          title: { type: 'String', required: false, default: el.definition.short },
-          description: { type: 'String', required: false, default: el.definition.formal },
-          required: el.definition.min == 1 
-        };
-      
+
+      var val = {
+        title: {
+          type: 'String',
+          required: false,
+          default: el.definition.short
+        },
+        description: {
+          type: 'String',
+          required: false,
+          default: el.definition.formal
+        },
+        required: el.definition.min == 1
+      };
+
       /**
         If you don't want title and description, comment out above and use below simple val def.
       **/
       //var val = {};
-      
-      if (el.definition.type[0].code == 'ResourceReference'){
-        
-        for(var i in el.definition.type) {
+
+      if (el.definition.type[0].code == 'ResourceReference') {
+
+        for (var i in el.definition.type) {
           //Need to clone it
           val = JSON.parse(JSON.stringify(val));
 
@@ -172,70 +187,74 @@ function processSchema(file) {
 
           updateObjectPath(obj, el.path + (i > 0 ? i : ''), el.definition.max == '*' ? [val] : val);
         }
-      }
-      else {
+      } else {
         var type = el.definition.type[0].code;
 
         //val[types[type] ? 'token' : 'type'] = types[type] ? types[type] : type;
-        
+
         //val.type = type;
 
-        if (types[type]){
+        if (types[type]) {
           //token types like Identifier
           //val.type = 'String';
           //_.extend(val, types[type]);
           //val.type = types[type];
-          for(var k in types[type]) {
+          for (var k in types[type]) {
             val[k] = types[type][k];
           }
           //delete required attribute
           delete val['required'];
-        }
-        else {
+        } else {
           val.type = type
         }
-        
+
         updateObjectPath(obj, el.path, el.definition.max == '*' ? [val] : val);
-      }      
-    }
-    else {
+      }
+    } else {
       updateObjectPath(obj, el.path, {});
     }
-    
+
   }
 
   obj = obj[model];
 
   //Extension types defined by organization
-  if (model != 'Extension'){
-    
+  if (model != 'Extension') {
+
     var dest = resourcesDest;
     if (path.basename(file).search(/^type/i) != -1) {
       dest = typesDest;
     }
-    if (path.basename(file).search(/^type-(period)/i) != -1){
+    if (path.basename(file).search(/^type-(period)/i) != -1) {
       dest = baseDest;
     }
 
     fs.writeFileSync(dest + '/' + model + '.json', JSON.stringify(obj, null, '  '));
   }
 
-  return { model: model, schema: obj };
+  return {
+    model: model,
+    schema: obj
+  };
 
 }
 
-function loadBase(folder){
+function loadBase(folder) {
   var files = fs.readdirSync(folder);
 
-  for(var i in files) {
-    var raw = fs.readFileSync(folder + '/' + files[i], {encoding:'utf8'});
+  for (var i in files) {
+    var raw = fs.readFileSync(folder + '/' + files[i], {
+      encoding: 'utf8'
+    });
     var json = JSON.parse(raw);
-    types[path.basename(files[i],'json')] = json;
-  } 
+    types[path.basename(files[i], 'json')] = json;
+  }
 }
 
 function loadTypes() {
-  var raw = fs.readFileSync('schemas/tmp/types.json', {encoding:'utf8'});
+  var raw = fs.readFileSync('schemas/tmp/types.json', {
+    encoding: 'utf8'
+  });
   var json = JSON.parse(raw);
   types = json;
 }
@@ -244,24 +263,28 @@ function processAllSchemas(folder) {
 
   var files = fs.readdirSync(folder);
 
-  for(var i in files) {
-    if (files[i].search(/profile\.json$/i) != -1){
+  for (var i in files) {
+    if (files[i].search(/profile\.json$/i) != -1) {
       processSchema(folder + '/' + files[i]);
     }
-    
-    if (files[i].search(/^valueset/i) != -1){
+
+    if (files[i].search(/^valueset/i) != -1) {
       processValueset(folder + '/' + files[i]);
     }
-    
-  } 
+
+  }
 
 }
 
 function flatTest() {
 
   var flatten = require('flat');
-  var specimen = fs.readFileSync('schemas/resources/Specimen.json',{encoding:'utf8'});
-  console.log(flatten(JSON.parse(specimen)), {safe:true});
+  var specimen = fs.readFileSync('schemas/resources/Specimen.json', {
+    encoding: 'utf8'
+  });
+  console.log(flatten(JSON.parse(specimen)), {
+    safe: true
+  });
 }
 
 setup();
--- server.js
+++ server.js
@@ -1,20 +1,21 @@
 // BASE SETUP
 // =============================================================================
-
 var _ = require('underscore');
 
 // call the packages we need
-var express    = require('express');
+var express = require('express');
 var bodyParser = require('body-parser');
-var app        = express();
+var app = express();
 
 // configure app
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
-var port     = process.env.PORT || 8080; // set our port
+var port = process.env.PORT || 8080; // set our port
 
-var mongoose   = require('mongoose');
+var mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/test1'); // connect to our database
 
 //Schema helper (load mongoose BEFORE schemaHelper)
@@ -22,8 +23,8 @@ var models = {};
 var schemaHelper = require('./lib/schema-helper');
 var modelsToLoad = ['base', 'types', 'resources', 'valuesets'];
 _.each(modelsToLoad, function(folder) {
-	var m = schemaHelper.loadSchemas(__dirname + '/schemas/'+folder);
-	_.extend(models, m);
+  var m = schemaHelper.loadSchemas(__dirname + '/schemas/' + folder);
+  _.extend(models, m);
 });
 
 
@@ -37,130 +38,139 @@ var router = express.Router();
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
-	// do logging
-	//console.log(req.query);
-	next();
+  // do logging
+  //console.log(req.query);
+  next();
 });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-	//res.json({ message: 'hooray! welcome to our api!' });
-	res.send('<pre>'+mongoose.modelNames().join('\n')+'</pre>');	
+  //res.json({ message: 'hooray! welcome to our api!' });
+  res.send('<pre>' + mongoose.modelNames().join('\n') + '</pre>');
 });
 
 var modelNames = mongoose.modelNames();
 _.each(modelNames, function(modelName) {
 
-//for (var i in modelNames) {
+  //for (var i in modelNames) {
 
-	//var modelName = modelNames[i];
-	var thisModel = models[modelName];
+  //var modelName = modelNames[i];
+  var thisModel = models[modelName];
 
-	// on routes that end in /modelName
-	// ----------------------------------------------------
-	router.route('/'+modelName)
+  // on routes that end in /modelName
+  // ----------------------------------------------------
+  router.route('/' + modelName)
 
-		// create a model (accessed at POST http://localhost:8080/modelName)
-		.post(function(req, res) {
-			
-			var model = new thisModel(req.body);		// create a new instance of the Bear model
+  // create a model (accessed at POST http://localhost:8080/modelName)
+  .post(function(req, res) {
 
-			model.save(function(err) {
-				if (err)
-					res.send(err);
+    var model = new thisModel(req.body); // create a new instance of the Bear model
 
-				res.json({ message: modelName + ' created!' });
-			});
+    model.save(function(err) {
+      if (err)
+        res.send(err);
 
-			
-		})
-
-		//Probably want to set default limit
-		// get all the fhir (accessed at GET http://localhost:8080/api/modelName)
-		
-		//http://www.hl7.org/implement/standards/modelName/search.html
-		//GET [base-url]/patient?_id=23
-		//GET [base-url]/Patient/23/procedure?date=>2010-01-01&date=<2011-12-31
-		//GET [base-url]/Patient?identifier=http://acme.org/patient|2345
-		//GET [base-url]/Patient?gender=http://hl7.org/modelName/v2/0001|M
-		//GET [base-url]/Patient?gender:text=male
-
-		.get(function(req, res) {
-			
-			//test
-			res.send(thisModel.schema);
-
-			/*
-			thisModel.find(function(err, doc) {
-				if (err)
-					res.send(err);
-
-				res.json(doc);
-			});
-			*/
-
-		});
-
-	// on routes that end in /modelName/:model_id
-	// ----------------------------------------------------
-	router.route('/modelName/:model_id')
-
-		// get the model with that id
-		.get(function(req, res) {
-			thisModel.findById(req.params.model_id, function(err, model) {
-				if (err)
-					res.send(err);
-				res.json(model);
-			});
-		})
-
-		// update the model with this id
-		.put(function(req, res) {
-			
-			thisModel.findByIdAndUpdate(req.params.model_id, req.body, {upsert:true,w:1})
-				.exec(function(err, doc) {
-					if (err)
-						res.send(err);
-					res.json({ message: modelName + ' updated!' });
-				});
-			/*
-			thisModel.findById(req.params.model_id, function(err, model) {
-
-				if (err)
-					res.send(err);
-
-				model.name = req.body.name;
-				model.save(function(err) {
-					if (err)
-						res.send(err);
-
-					res.json({ message: modelName + ' updated!' });
-				});
-
-			});
-			*/
-		})
-
-		// delete the model with this id
-		.delete(function(req, res) {
-			thisModel.remove({
-				_id: req.params.model_id
-			}, function(err, model) {
-				if (err)
-					res.send(err);
-
-				res.json({ message: 'Successfully deleted' });
-			});
-		});
+      res.json({
+        message: modelName + ' created!'
+      });
+    });
 
 
-	// REGISTER OUR ROUTES -------------------------------
-	app.use('/fhir', router);
+  })
+
+  //Probably want to set default limit
+  // get all the fhir (accessed at GET http://localhost:8080/api/modelName)
+
+  //http://www.hl7.org/implement/standards/modelName/search.html
+  //GET [base-url]/patient?_id=23
+  //GET [base-url]/Patient/23/procedure?date=>2010-01-01&date=<2011-12-31
+  //GET [base-url]/Patient?identifier=http://acme.org/patient|2345
+  //GET [base-url]/Patient?gender=http://hl7.org/modelName/v2/0001|M
+  //GET [base-url]/Patient?gender:text=male
+
+  .get(function(req, res) {
+
+    //test
+    res.send(thisModel.schema);
+
+    /*
+    thisModel.find(function(err, doc) {
+    	if (err)
+    		res.send(err);
+
+    	res.json(doc);
+    });
+    */
+
+  });
+
+  // on routes that end in /modelName/:model_id
+  // ----------------------------------------------------
+  router.route('/modelName/:model_id')
+
+  // get the model with that id
+  .get(function(req, res) {
+    thisModel.findById(req.params.model_id, function(err, model) {
+      if (err)
+        res.send(err);
+      res.json(model);
+    });
+  })
+
+  // update the model with this id
+  .put(function(req, res) {
+
+    thisModel.findByIdAndUpdate(req.params.model_id, req.body, {
+        upsert: true,
+        w: 1
+      })
+      .exec(function(err, doc) {
+        if (err)
+          res.send(err);
+        res.json({
+          message: modelName + ' updated!'
+        });
+      });
+    /*
+    thisModel.findById(req.params.model_id, function(err, model) {
+
+    	if (err)
+    		res.send(err);
+
+    	model.name = req.body.name;
+    	model.save(function(err) {
+    		if (err)
+    			res.send(err);
+
+    		res.json({ message: modelName + ' updated!' });
+    	});
+
+    });
+    */
+  })
+
+  // delete the model with this id
+  .delete(function(req, res) {
+    thisModel.remove({
+      _id: req.params.model_id
+    }, function(err, model) {
+      if (err)
+        res.send(err);
+
+      res.json({
+        message: 'Successfully deleted'
+      });
+    });
+  });
+
+
+  // REGISTER OUR ROUTES -------------------------------
+  app.use('/fhir', router);
 
 });
 
-app.get('/', function(req, res){
-	res.send('Try http://localhost:8080/fhir instead');
+app.get('/', function(req, res) {
+  res.send('Try http://localhost:8080/fhir instead');
 });
 
 // START THE SERVER
