7aa1085191705021e4b758d8c35e952afd0d73ee
htaox <htaox@hotmail.com>
2016-01-06 18:50:22 -0500

    axios.create not supported yet per author

--- src/helpers/ApiClient.js
+++ src/helpers/ApiClient.js
@@ -44,30 +44,11 @@ class _ApiClient {
           config.data = data;
         }
 
-        return axios.create(config);
+        return axios(config);
 
       };     
 
-      /*
-      this[method] = (path, { params, data } = {}) => new Promise((resolve, reject) => {
-        const request = superagent[method](formatUrl(path));
-
-        if (params) {
-          request.query(params);
-        }
-
-        if (__SERVER__ && req.get('cookie')) {
-          request.set('cookie', req.get('cookie'));
-        }
-
-        if (data) {
-          request.send(data);
-        }
-
-        request.end((err, { body } = {}) => err ? reject(body || err) : resolve(body));
-        */
-     
-      });
+    });
       
   }
 }
